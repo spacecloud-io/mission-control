9406d8d5790a7de2439e8ded27eb927ef74f1774
Jayesh Choudhary <jayesh@spaceuptech.com>
2019-10-23 22:01:13 +0530

    changed default connection string of mysql

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@ export const dbTypes = {
 export const defaultDbConnectionStrings = {
   [dbTypes.MONGO]: "mongodb://localhost:27017",
   [dbTypes.POSTGRESQL]: "postgres://postgres:mysecretpassword@localhost/postgres?sslmode=disable",
-  [dbTypes.MYSQL]: "user:my-secret-pwd@/"
+  [dbTypes.MYSQL]: "root:my-secret-pw@tcp(localhost:3306)/"
 }
 
 export const SPACE_CLOUD_USER_ID = "internal-sc-user"
