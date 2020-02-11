08098e87acabb1cc33d784348def0f511e5a7a9e
Saif-Ali-Kamal <saifali.sak13@gmail.com>
2020-02-11 14:02:11 +0530

    change trigger to triggers

--- src/services/eventing.js
+++ src/services/eventing.js
@@ -33,7 +33,7 @@ class Eventing {
 
   setTriggerRule(projectId, triggerName, triggerRule) {
     return new Promise((resolve, reject) => {
-      this.client.postJSON(`/v1/config/projects/${projectId}/eventing/trigger/${triggerName}`, triggerRule)
+      this.client.postJSON(`/v1/config/projects/${projectId}/eventing/triggers/${triggerName}`, triggerRule)
         .then(({ status, data }) => {
           if (status !== 200) {
             reject(data.error)
@@ -47,7 +47,7 @@ class Eventing {
 
   deleteTriggerRule(projectId, triggerName) {
     return new Promise((resolve, reject) => {
-      this.client.delete(`/v1/config/projects/${projectId}/eventing/trigger/${triggerName}`)
+      this.client.delete(`/v1/config/projects/${projectId}/eventing/triggers/${triggerName}`)
         .then(({ status, data }) => {
           if (status !== 200) {
             reject(data.error)
