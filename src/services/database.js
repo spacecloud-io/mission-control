433a011db585556c48885cd853688b982e44e006
Shubham Nazare <shubham4443@gmail.com>
2020-03-22 15:04:57 +0530

    Changes in request payload for some routes (#118)

--- src/services/database.js
+++ src/services/database.js
@@ -14,7 +14,7 @@ class Database {
             reject(data.error)
             return
           }
-          resolve(data.status)
+          resolve(data.result)
         })
         .catch(ex => reject(ex.toString()))
     })
@@ -56,7 +56,7 @@ class Database {
             reject(data.error)
             return
           }
-          resolve(data.collections)
+          resolve(data.result)
         })
         .catch(ex => reject(ex.toString()))
     })
@@ -84,7 +84,7 @@ class Database {
             reject(data.error)
             return
           }
-          resolve(data.collections)
+          resolve(data.result)
         })
         .catch(ex => reject(ex.toString()))
     })
@@ -112,7 +112,7 @@ class Database {
             reject(data.error)
             return
           }
-          resolve(data.schema)
+          resolve(data.result)
         })
         .catch(ex => reject(ex.toString()))
     })
@@ -134,7 +134,9 @@ class Database {
 
   setColRule(projectId, dbName, colName, rule) {
     return new Promise((resolve, reject) => {
-      this.client.postJSON(`/v1/config/projects/${projectId}/database/${dbName}/collections/${colName}/rules`, rule)
+      this.client.postJSON(`/v1/config/projects/${projectId}/database/${dbName}/collections/${colName}/rules`, {
+        isRealtimeEnabled: typeof rule.isRealtimeEnabled === "string" ? false : rule.isRealtimeEnabled, ...rule
+      })
         .then(({ status, data }) => {
           if (status !== 200) {
             reject(data.error)
--- src/services/projects.js
+++ src/services/projects.js
@@ -11,15 +11,15 @@ class Projects {
             reject(data.error)
             return
           }
-          resolve(data)
+          resolve(data.result)
         })
         .catch(ex => reject(ex.toString()))
     })
   }
 
-  addProject(projectId, config) {
+  addProject(projectId, projectConfig) {
     return new Promise((resolve, reject) => {
-      this.client.postJSON(`/v1/config/projects/${projectId}`, config)
+      this.client.postJSON(`/v1/config/projects/${projectId}`, projectConfig)
         .then(({ status, data }) => {
           if (status !== 200) {
             reject(data.error)
