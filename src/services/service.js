1a0f9f71a828903eec0ac7232ab8f0c045bad674
Jayesh333 <jayesh@spaceuptech.com>
2020-05-04 20:58:47 +0530

    minor changes

--- src/services/service.js
+++ src/services/service.js
@@ -97,9 +97,9 @@ class Service {
     })
   }
 
-  setClusterIdentity(clusterId, key) {
+  setClusterIdentity(clusterId, clusterKey) {
     return new Promise((resolve, reject) => {
-      this.client.postJSON("/v1/config/upgrade", { clusterId, key })
+      this.client.postJSON("/v1/config/upgrade", { clusterId, clusterKey })
         .then(({ status, data }) => {
           if (status != 200) {
             reject(data.error)
