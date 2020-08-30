ca75d965e4bff4d3ae0f444e71b55ba52bf4ccb8
Jayesh333 <jayesh@spaceuptech.com>
2020-08-30 15:06:59 +0530

    changed license field name

--- src/services/cluster.js
+++ src/services/cluster.js
@@ -60,9 +60,9 @@ class Cluster {
     })
   }
 
-  setOfflineClusterLicense(licenseKey) {
+  setOfflineClusterLicense(license) {
     return new Promise((resolve, reject) => {
-      this.client.postJSON("/v1/config/offline-license", {licenseKey})
+      this.client.postJSON("/v1/config/offline-license", { license })
         .then(({ status, data }) => {
           if (status < 200 || status >= 300) {
             reject(data.error)
