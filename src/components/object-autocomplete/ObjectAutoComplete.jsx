6fb76334bada13d90a0fec3371a87b19818336f2
shubham4443 <shubham4443@gmail.com>
2020-09-15 20:50:50 +0530

    fixed the bug

--- src/components/object-autocomplete/ObjectAutoComplete.jsx
+++ src/components/object-autocomplete/ObjectAutoComplete.jsx
@@ -1,53 +1,46 @@
-import React, {useState, useEffect} from 'react';
-import {AutoComplete} from 'antd';
+import React, { useState, useEffect } from 'react';
+import { AutoComplete } from 'antd';
 import dotProp from 'dot-prop-immutable';
 
-
-let data = [];
 const ObjectAutoComplete = (props) => {
-  const {options} = props; 
+  const { options } = props;
   const [value, setValue] = useState(props.value ? props.value : "");
-  useEffect(() => {
-    data = Object.keys(options);
-  }, [props])
 
-  const changeOptions = path => {
-    data = dotProp.get(options, path) ? Object.keys(dotProp.get(options, path)) : Object.keys(options);
+  const changeOptions = () => {
+    const variables = value.split(".");
+    const path = variables.slice(0, variables.length - 1);
+
+    return dotProp.get(options, path.join(".")) ? Object.keys(dotProp.get(options, path.join("."))) : Object.keys(options);
   }
+
+  const data = changeOptions();
 
   const handleSearch = (val) => {
     setValue(val);
     props.onChange(val);
-    const variables = val.split(".");
-    let path = "";
-    for (let i = 0; i < variables.length - 1; i++) {
-      if (i === 0) path += variables[i];
-      else path += "." + variables[i];
-    }
-    changeOptions(path);
   }
-
+  
   const handleSelect = (val) => {
     setValue(value.substring(0, value.lastIndexOf(".") + 1) + val); // Before last dot
     props.onChange(value.substring(0, value.lastIndexOf(".") + 1) + val);
   }
   return (
     <AutoComplete
-     placeholder={props.placeholder}
-     onSearch={handleSearch}
-     onSelect={handleSelect}
-     value={value}
+      placeholder={props.placeholder}
+      onSearch={handleSearch}
+      onSelect={handleSelect}
+      value={value}
     >
-     {data
-       .filter(
-         (data) =>
-           data.toLowerCase().indexOf(value.substring(value.lastIndexOf(".") + 1).toLowerCase()) !== -1 // After last dot
-       )
-       .map((data) => (
-         <AutoComplete.Option key={data} value={data}>
-           {data}
-         </AutoComplete.Option>
-       ))}
+      {data
+        .filter(
+          (data) =>
+            data.toLowerCase().indexOf(value.substring(value.lastIndexOf(".") + 1).toLowerCase()) !== -1 // After last dot
+        )
+        .map((data) => (
+          <AutoComplete.Option key={data} value={data}>
+            {data}
+          </AutoComplete.Option>
+        ))}
     </AutoComplete>
   )
 }
