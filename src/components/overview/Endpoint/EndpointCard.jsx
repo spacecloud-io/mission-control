26cadbabd6ae72d3e23588bd48b6062e57cd6614
shubham4443 <shubham4443@gmail.com>
2021-03-14 02:47:42 +0530

    wip

--- src/components/overview/Endpoint/EndpointCard.jsx
+++ src/components/overview/Endpoint/EndpointCard.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
-import { Card, Typography } from 'antd';
+import { Card, Col, Row, Typography } from 'antd';
 import { getGraphQLEndpoints } from '../../../utils';
+import { DatabaseOutlined } from '@ant-design/icons';
+
+const cardStyles = {
+	overflow: "hidden",
+	border: "0.3px solid #C4C4C4",
+	boxShadow: "-2px -2px 8px rgba(0, 0, 0, 0.15), 2px 2px 8px rgba(0, 0, 0, 0.15)",
+	borderRadius: "5px"
+}
 
 
 const EndpointCard = ({ projectId }) => {
 	const { httpUrl, websocketUrl } = getGraphQLEndpoints(projectId)
 
 	return (
-		<Card style={{ overflow: "auto" }}>
-			<Typography.Text strong>HTTP URL:</Typography.Text><Typography.Text copyable ellipsis>{httpUrl}</Typography.Text>
-			<br />
-			<Typography.Text strong>Websocket URL:</Typography.Text><Typography.Text copyable ellipsis>{websocketUrl}</Typography.Text>
+		<Card style={cardStyles}>
+			<Row gutter={100}>
+				<Col style={{ borderRight: "0.3px solid darkgrey" }}>
+					<h1 style={{ margin: 0, padding: 0 }}><DatabaseOutlined /> 04</h1>
+					<h3 style={{ margin: 0, padding: 0, color: "darkgrey" }}>Databases</h3>
+				</Col>
+				<Col style={{ borderRight: "0.3px solid darkgrey" }}>
+					<h1 style={{ margin: 0, padding: 0 }}><DatabaseOutlined /> 14</h1>
+					<h3 style={{ margin: 0, padding: 0, color: "darkgrey" }}>Microservices</h3>
+				</Col>
+				<Col style={{ borderRight: "0.3px solid darkgrey" }}>
+					<h1 style={{ margin: 0, padding: 0 }}><DatabaseOutlined /> 06</h1>
+					<h3 style={{ margin: 0, padding: 0, color: "darkgrey" }}>Users</h3>
+				</Col>
+				<Col style={{ alignSelf: "center", lineHeight: "24px" }}>
+					<Typography.Text strong>HTTP URL:</Typography.Text><Typography.Text copyable ellipsis>{httpUrl}</Typography.Text>
+					<br />
+					<Typography.Text strong>Websocket URL:</Typography.Text><Typography.Text copyable ellipsis>{websocketUrl}</Typography.Text>
+				</Col>
+			</Row>
 		</Card>
 	);
 }
--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -9,10 +9,17 @@ import EndpointCard from '../../components/overview/Endpoint/EndpointCard';
 import DiscordCard from '../../components/overview/discord/DiscordCard';
 import GithubCard from '../../components/overview/github/GithubCard';
 import UpgradeCard from '../../components/overview/upgrade/UpgradeCard';
-import { Row, Col } from 'antd';
+import { Row, Col, Card, Select } from 'antd';
 import { isClusterUpgraded } from '../../operations/cluster';
 import { openBillingPortal } from '../../utils';
 import { projectModules } from '../../constants';
+
+const cardStyles = {
+  overflow: "hidden",
+  border: "0.3px solid #C4C4C4",
+  boxShadow: "-2px -2px 8px rgba(0, 0, 0, 0.15), 2px 2px 8px rgba(0, 0, 0, 0.15)",
+  borderRadius: "5px"
+}
 
 function Overview() {
   const { projectID } = useParams()
@@ -25,16 +32,59 @@ function Overview() {
       <Sidenav selectedItem={projectModules.OVERVIEW} />
       <div className="page-content ">
         <Row>
-          <Col lg={{ span: 20 }} sm={{ span: 24 }}>
-            <h3>GraphQL Endpoints</h3>
+          <Col span={24}>
+            <h3 style={{ marginBottom: 24 }}>Welcome Vinay Parab to Pegasis Mission Control</h3>
 
             <EndpointCard projectId={projectID} />
-            <h3 style={{ marginTop: 24 }}>Community</h3>
-            <Row>
-              <Col lg={{ span: 11, offset: 0 }}>
+            <Row gutter={[32, 16]} style={{ marginTop: 24 }}>
+              <Col xl={8} sm={24}>
+                <Card style={cardStyles}>
+                  <h3 style={{ marginBottom: 16 }}>API requests handled</h3>
+                  <Row>
+                    <Col style={{ paddingRight: 32, borderRight: "0.3px solid darkgrey" }}>
+                      <h1 style={{ margin: 0, padding: 0 }}>32</h1>
+                      <h3 style={{ margin: 0, padding: 0, color: "darkgrey" }}>Ingress</h3>
+                    </Col>
+                    <Col style={{ paddingLeft: 32 }}>
+                      <h1 style={{ margin: 0, padding: 0 }}>18</h1>
+                      <h3 style={{ margin: 0, padding: 0, color: "darkgrey" }}>GraphQL</h3>
+                    </Col>
+                  </Row>
+                </Card>
+              </Col>
+              <Col xl={6} sm={24}>
+                <Card style={cardStyles}>
+                  <h3 style={{ marginBottom: 16 }}>Error Rate</h3>
+                  <h1 style={{ margin: 0, padding: 0 }}>03</h1>
+                  <h3 style={{ margin: 0, padding: 0, color: "darkgrey" }}>Errors</h3>
+                </Card>
+              </Col>
+              <Col xl={10} sm={24}>
+                <Card style={{ height: "100%" }}>
+                  <div style={{ fontSize: 14, marginBottom: 8 }}><b>Billable resources of Pegasis</b></div>
+                  <Row>
+                    <Col span={12} style={{ borderRight: "0.3px solid darkgrey" }}>
+                      <p style={{ marginBottom: 0, lineHeight: "24px" }}>
+                        Database: 4 <br />
+                        RAM: 4GB <br />
+                        Server space: 6GB <br />
+                      </p>
+                    </Col>
+                    <Col span={12}>
+                      <p style={{ marginBottom: 0, lineHeight: "24px", marginLeft: 24 }}>
+                        Estimated Billing this month
+                        <h3>$ 18.3</h3>
+                      </p>
+                    </Col>
+                  </Row>
+                </Card>
+              </Col>
+            </Row>
+            <Row gutter={[32, 16]} style={{ marginTop: 24 }}>
+              <Col xl={7} sm={24}>
                 <DiscordCard />
               </Col>
-              <Col lg={{ span: 11, offset: 2 }} className="git-card">
+              <Col xl={7} sm={24} className="git-card">
                 <GithubCard />
               </Col>
             </Row>
