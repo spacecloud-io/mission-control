fa9cdd7b6762ce6f91946980e2fbe783acd170fd
YourTechBud <noorain@spaceuptech.com>
2021-04-29 12:50:08 +0530

    Added scripts

---
+++ scripts/push.sh
@@ -0,0 +1,7 @@
+rm -rf build/
+rm ./mission-control.zip
+npm i
+npm run build
+zip mission-control.zip -r ./build/
+gsutil rm gs://space-cloud/mission-control/mission-control-v0.21.4.zip
+gsutil cp ./mission-control.zip gs://space-cloud/mission-control/mission-control-v0.21.4.zip
